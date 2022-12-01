dcfd9115f52bb0a1e6335478522250a0b4db3c6f
Ronan Limon Duparcmeur <ronan@2-45.pm>
2022-12-01 20:45:21 +0100

    Parsing the data gets the right amount of elves
    … and the calories they carry are probably well parsed, too!

--- 01/lib.rb
+++ 01/lib.rb
@@ -2,8 +2,22 @@ class Inventory
   attr_reader :elves
   
   def initialize(data)
-    @data  = data
-    @elves = []
+    parse_data data
+  end
+  
+  private
+  
+  def parse_data(data)
+    @elves = data
+               .chomp!
+               .split(/\n{2}/)
+               .map(&:split)
+               .map { |cc| Elf.new(calories_carried: cc) }
   end
 end
+
+class Elf
+  def initialize(calories_carried: [])
+    @calories_carried = calories_carried
+  end
 end
