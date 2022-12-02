45ad792adc4b4785f3dda37aa34a2e5efb65baa3
Ronan Limon Duparcmeur <ronan@2-45.pm>
2022-12-02 07:49:47 +0100

    Parsing the input data
    Even for me, this is overkill. Like some kind of orchestral intro in a Guns N' Roses
    song.

--- 02/strategy.rb
+++ 02/strategy.rb
@@ -1,2 +1,11 @@
 class Strategy
+  attr_reader :rounds
+  
+  def self.from_data(data)
+    new data.split(/\n/)
+  end
+  
+  def initialize(rounds = [])
+    @rounds = rounds
+  end
 end
--- 02/tests.rb
+++ 02/tests.rb
@@ -2,4 +2,17 @@ require "minitest/autorun"
 
 require_relative "strategy"
 class StrategyTest < Minitest::Test
+  def setup
+    @data = <<~TXT
+      A Y
+      B X
+      C Z
+    TXT
+  end
+  
+  def test_parsing_input_data
+    strategy = Strategy.from_data(@data)
+    
+    assert_equal 3, strategy.rounds.count
+  end
 end
