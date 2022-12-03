1e95bac7e32d7158838fcf1824c369b6cc88f68d
Ronan Limon Duparcmeur <ronan@2-45.pm>
2022-12-03 16:11:01 +0100

    Test + implementation for the badges detectors
    All in a single commit, but the tests were written first!

--- 03/rucksacks_sorter.rb
+++ 03/rucksacks_sorter.rb
@@ -7,6 +7,7 @@ class RucksacksSorter
   
   def initialize(*sacks_contents)
     @misplaced_item_detectors = sacks_contents.map { |sc| detector_for(sc) }
+    @badges_detectors = sacks_contents.each_slice(3).map { |group_contents| badge_detector_for(group_contents) }
   end
   
   def misplaced_items_priorities_sum
@@ -21,12 +22,31 @@ class RucksacksSorter
     @_misplaced_items ||= @misplaced_item_detectors.map(&:take)
   end
   
+  def badges_priorities_sum
+    badges_priorities.sum
+  end
+  
+  def badges_priorities
+    badges.map { |i| ITEMS_BY_PRIORITY.index(i) + 1 }
+  end
+  
+  def badges
+    @_badges ||= @badges_detectors.map(&:take)
+  end
+  
   private
   
   def detector_for(sack_contents)
     Ractor.new(sack_contents) do |sc|
       first_compartment, second_compartment = sc.chars.each_slice(sc.length / 2).to_a
       (first_compartment & second_compartment).first
+    end
+  end
+  
+  def badge_detector_for(group_contents)
+    Ractor.new(group_contents) do |gc|
+      a, b, c = gc.map(&:chars)
+      (a & b & c).first
     end
   end
 end
